c90b5411114d849ecefd007c0a5ade0aabac7e49
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2026-02-09 14:09:12 +0530

    Update page.tsx

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -1,9 +1,11 @@
 import AboutUsPage from "@/components/AboutUsPage";
+import AboutUsPage from "@/components/Ourteam";
 export default function AboutusPage() {
     return(
         <>
         
        <AboutUsPage />
+            <Ourteam />
         
         </>
     );
