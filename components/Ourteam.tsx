c5ae0a76899dc31291fae45c3974764b2e75b2f2
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2026-02-09 15:02:55 +0530

    Update Ourteam.tsx

--- components/Ourteam.tsx
+++ components/Ourteam.tsx
@@ -35,7 +35,7 @@ export default function Ourteam() {
         <div className="max-w-6xl mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-10">Meet Our Team</h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-6xl py-50">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-6xl px-50">
             {team.map((member) => (
               <div
                 key={member.name}
