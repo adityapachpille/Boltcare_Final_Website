23864f39da283aa2e15201cecacde1ecbf34a731
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2026-05-29 17:01:41 +0530

    Update contact email for career-related queries

--- components/Careers.tsx
+++ components/Careers.tsx
@@ -97,7 +97,7 @@ const Careers = () => {
 
                <li className="mt-14">
                For career-related queries, email us at  <a href="mailto:boltcarepharma@gmail.com" className="text-blue-600">
-                    boltcarepharma@gmail.com
+                    connect.boltcarepharma@gmail.com
                   </a> or <br></br> message us on WhatsApp at  <a href="https://wa.me/9763714046" aria-label="WhatsApp" target="_blank" className="text-blue-600">
  9763714046.
                 </a>
